f976252cd08a726ea1317face6b2c1909a9322e4
marcozsouto <marcozsouto@gmail.com>
2021-06-24 22:42:56 -0300

    Atualizando tela jogo

--- views/MainGame.pl
+++ views/MainGame.pl
@@ -1,26 +1,51 @@
 use Tk;
 use strict;
 
+
+#variaveis globais
 my $main = MainWindow->new;
 my $top;
+my $acc_water = 0;
+my $acc_ship = 0;
+my $qnt_ship = 0;
+
 my @matriz_ENEMY = (
-        [1, 0, 0],
-        [1, 0, 0],
-        [2, 2, 2]);
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 
 my @matriz_MY = (
-        [1, 0, 0],
-        [1, 0, 0],
-        [2, 2, 2]);
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 sub start{
-    $main->minsize(qw(600 600));
+    $main->minsize(qw(1100 900));
     $main->title("Batalha naval");
     $main->configure(-background => "white");
 
     $top = $main->Frame(-background => "white");
     $top->pack(-side => 'top', -fill => 'x');
+
+    plotMatrix(\@matriz_ENEMY, 'unblocked');
+    plotMatrix(\@matriz_MY, 'blocked');
+    
+    MainLoop();
 }
 
 sub plotMatrix  {
@@ -35,7 +60,7 @@ sub plotMatrix  {
     for(my $i = 0; $i <= $#final; $i++){
 
         #Criar Frame
-        my $left = $top->Frame(-background => "red");
+        my $left = $top->Frame(-background => "white");
         $left->pack(-side => 'left', -pady => 1, -padx => 1);
 
         for(my $j = 0; $j <= $#final ; $j++){ 
@@ -44,11 +69,18 @@ sub plotMatrix  {
             
             if($param ne 'blocked'){
                 $buttons[$i][$j]->configure(-command => [\&click, $j, $i, $buttons[$i][$j]]);
+
+                if($final[$i][$j] != 0){
+                    $qnt_ship = $qnt_ship + 1;
+                }
             }
             
             $buttons[$i][$j]->pack();  
-        }
+        }   
     }   
+}
+
+sub ganhou {
 
 }
 
@@ -60,15 +92,21 @@ sub click {
     my $valor = 0;
     
     #atualizar matriz
-    $clicked[2]->configure(-text=>"5");
+    $clicked[2]->configure(-text=>"$valor");
 
     if($valor == 0){
-                
+        $acc_water = $acc_water + 1;
     }
+    else{
+        $acc_ship = $acc_ship + 1; 
+        
+        if($acc_ship == $qnt_ship){
+            ganhou();
+        }else{
+
+        }
+    }
+
 }
 
-
 start();
-plotMatrix(\@matriz_ENEMY, 'unblocked');
-plotMatrix(\@matriz_MY, 'blocked');
-MainLoop();
